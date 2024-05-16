6e8b514593660f1d39a1c939a6aad13c2c5d0b86
Rous25-1 <rosyarimana@gmail.com>
2024-05-16 08:08:29 -0500

    nosotros
    actualizacion

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title> Carrito de Compras</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="MyraStudio" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="plantilla/Admin/vertical/assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+        <div class="header-border"></div>
+        
+        <?php
+        include("include/menu.php");
+        ?>
+
+
+
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+
+            <div class="page-content">
+                <div class="container-fluid">
+                    <div class="row">
+                    <div class="col-12">
+                            <div class="card">
+                                <div class="card-body">
+                    
+                                    <h4 class="card-title">With Indicators</h4>
+                                    <p class="card-subtitle mb-4">You can also add the indicators to the carousel, alongside the controls, too.</p>
+                                    
+                                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                        </ol>
+                                        <div class="carousel-inner" role="listbox">
+                                            <div class="carousel-item active">
+                                                <img class="d-block img-fluid" src="img/portada.jpg" alt="First slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid" src="img/portada.jpg" alt="Second slide">
+                                            </div>
+                                            <div class="carousel-item">
+                                                <img class="d-block img-fluid" src="img/portada.jpg" alt="Third slide">
+                                            </div>
+                                        </div>
+                                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Previous</span>
+                                        </a>
+                                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                            <span class="sr-only">Next</span>
+                                        </a>
+                                    </div>
+                                </div> <!-- end card-body-->
+                            </div> <!-- end card-->
+                        </div> <!-- end col -->
+                    </div>
+
+                    <!-- start page title -->
+                    <div class="row">
+                        <div class="col-12">
+                            
+                        </div>
+                    </div>
+                    <!-- end page title -->
+                    
+                    <div class="page-title-box d-flex aling-items-center justify-content-between">
+                        <div class="container-fluid row">
+                            <?php for ($i = 0; $i < 10; $i++) {
+                            ?>
+                                <div class="col-lg-2 col-md-3 col-sm-6">
+                                    <div class="card">
+                                        <img class="card-img-top img-fluid" src="img/zx.jpg" alt="">
+                                        <div class="card-body">
+                                            <h5 class="card-title">titulo</h5>
+                                            <p class="card-text">
+                                                descripcion
+                                            </p>
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            <?php } ?>
+
+                        </div>
+
+                    </div>
+                    </div>
+
+                    <?php
+                    include("include/pie_pagina.php");
+                    ?>
+
+
+
+                </div>
+                <!-- end main content-->
+
+
+
+
+
+
+            </div>
+            <!-- END layout-wrapper -->
+
+            <!-- Overlay-->
+            <div class="menu-overlay"></div>
+
+
+            <!-- jQuery  -->
+            <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
+            <script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+            <script src="plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
+            <script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
+            <script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
+
+            <!-- App js -->
+            <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
+
+</body>
+
+</html>
---
+++ nosotros.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title> Xeloro - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="MyraStudio" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="plantilla/Admin/vertical/assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+    <?php include("include/menu.php"); ?>
+
+    <!-- ============================================================== -->
+    <!-- Start right Content here -->
+    <!-- ============================================================== -->
+    <div class="main-content">
+
+        <div class="page-content">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-4"></div>
+                    <div class="col-sm-8">
+                        <div class="text-center mt-4">
+                            <h4>Historia de Nuestra Empresa</h4>
+                            <p class="text-center"> Tienda 5A nació de la pasión por la moda y el deseo de ofrecer a los clientes una experiencia única de compra. Fundada en la pintoresca ciudad de San Isidro hace más de dos décadas, Tienda 5A comenzó como una modesta boutique en una calle lateral, con un enfoque claro en la calidad y la atención al detalle.
+                                <li>
+                                    La fundadora, María González, una apasionada diseñadora de moda, siempre soñó con crear una marca que no solo ofreciera ropa de alta calidad, sino que también transmitiera un mensaje de estilo, elegancia y responsabilidad social. Con este objetivo en mente, María comenzó a viajar por el mundo en busca de las mejores telas y artesanos expertos que pudieran ayudar a hacer realidad su visión.
+                                </li>
+                                <li>
+                                    Con el paso de los años, Tienda 5A se ganó la confianza y el aprecio de la comunidad local, gracias a su compromiso con la calidad, el diseño único y el excelente servicio al cliente. La boutique se convirtió en un destino favorito para los amantes de la moda en San Isidro y sus alrededores, atrayendo a clientes de todas partes con su selección cuidadosamente curada de prendas de vestir y accesorios.
+                                </li>
+                                <li>
+                                    A medida que la marca ganaba reconocimiento, Tienda 5A expandió sus operaciones, abriendo nuevas tiendas en diferentes ciudades y lanzando una tienda en línea para llegar a un público más amplio. A pesar de su crecimiento, la empresa nunca perdió de vista sus valores fundamentales: la calidad, la sostenibilidad y la conexión con la comunidad.
+                                </li>
+                                <li>
+                                    Hoy en día, Tienda 5A se ha convertido en una marca de renombre nacional, conocida por su estilo distintivo y su compromiso con la moda ética. Desde su humilde comienzo en San Isidro hasta su posición como líder en la industria de la moda, Tienda 5A sigue siendo fiel a su misión original de inspirar a las personas a expresarse a través de la moda, mientras deja una huella positiva en el mundo que la rodea.
+                                </li>
+                            </p>
+
+                        </div>
+                    </div><!-- end col -->
+
+                    
+                </div><!-- end row -->
+                <br>
+                <br>
+                <br>
+                <br>
+                <!-- end row-->
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-xl-8">
+                        <div class="card">
+                            <div class="card-body">
+
+                                <h4 class="card-title">Vision, Mision y Valores</h4>
+
+                                <ul class="nav nav-tabs mb-3">
+                                    <li class="nav-item">
+                                        <a href="#home" data-toggle="tab" aria-expanded="false" class="nav-link">
+                                            <i class="mdi mdi-home-variant d-lg-none d-block"></i>
+                                            <span class="d-none d-lg-block">Vision</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#profile" data-toggle="tab" aria-expanded="true" class="nav-link active">
+                                            <i class="mdi mdi-account-circle d-lg-none d-block"></i>
+                                            <span class="d-none d-lg-block">Mision</span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#settings" data-toggle="tab" aria-expanded="false" class="nav-link">
+                                            <i class="mdi mdi-settings-outline d-lg-none d-block"></i>
+                                            <span class="d-none d-lg-block">Valores</span>
+                                        </a>
+                                    </li>
+                                </ul>
+
+                                <div class="tab-content">
+                                    <div class="tab-pane" id="home">
+                                        <p>"Nos visualizamos como la principal influencia en la industria de la moda, redefiniendo constantemente los estándares de estilo y calidad. Buscamos trascender las fronteras geográficas, convirtiéndonos en un referente global en moda sostenible y ética. Nuestra visión abarca la creación de un impacto positivo en la sociedad y el medio ambiente, sirviendo como ejemplo de excelencia empresarial y compromiso con la comunidad. En nuestra búsqueda de la excelencia, aspiramos a inspirar a nuestros clientes a través de la innovación, la creatividad y la pasión por la moda, manteniendo siempre nuestra integridad y respeto por todas las personas y recursos involucrados en nuestro proceso de producción y distribución."</p>
+                                    </div>
+                                    <div class="tab-pane show active" id="profile">
+                                        <p>"En TIENDA 5A, nos comprometemos a ofrecer a nuestros clientes productos de moda de alta calidad que reflejen su estilo y personalidad. Nos esforzamos por estar a la vanguardia de las últimas tendencias, mientras mantenemos un enfoque ético y sostenible en todas nuestras operaciones. Además, nos dedicamos a crear un ambiente de trabajo inclusivo donde nuestro equipo pueda crecer y prosperar."</p>
+                                    </div>
+                                    <div class="tab-pane" id="settings">
+                                        <p>
+                                            <li>
+                                                Calidad: Nos comprometemos a ofrecer productos de moda de la más alta calidad, desde los materiales hasta la fabricación y el diseño.
+                                            </li>
+                                            <li>
+                                                Innovación: Buscamos constantemente nuevas formas de mejorar y adaptarnos a las tendencias cambiantes del mercado, manteniendo nuestra marca fresca y relevante.
+                                            </li>
+                                            <li>
+                                                Sostenibilidad: Nos comprometemos a minimizar nuestro impacto en el medio ambiente mediante prácticas de fabricación sostenibles y el uso de materiales ecoamigables siempre que sea posible.
+                                            </li>
+                                            <li>
+                                                Ética: Operamos con integridad en todas nuestras interacciones, desde nuestras relaciones con los proveedores hasta nuestro compromiso con la equidad laboral y la transparencia.
+                                            </li>
+                                            <li>
+                                                Pasión por la moda: Amamos la moda y nos apasiona ayudar a nuestros clientes a expresarse a través de su estilo personal. Nos esforzamos por transmitir esta pasión en todo lo que hacemos, desde el diseño hasta el servicio al cliente.
+                                            </li>
+                                        </p>
+                                    </div>
+                                </div>
+
+                            </div> <!-- end card-body-->
+                        </div> <!-- end card-->
+                    </div> <!-- end col -->
+                    <div class="col-2"></div>
+                </div>
+                <!-- end row-->
+                <div class="row">
+                    <div class="col-2"></div>
+                    <div class="col-xl-8">
+                        <br>
+                        <h1>Trabajadores</h1>
+                        <div class="page-title-box d-flex aling-items-center justify-content-between">
+                            <div class="container-fluid row">
+                                <?php for ($i = 0; $i < 4; $i++) {
+                                ?>
+                                    <div class=" col-md-3">
+                                        <div class="card">
+                                            <img class="card-img-top img-fluid" src="img/m.avif" alt="">
+                                            <div class="card-body">
+                                                <h5 class="card-title">titulo</h5>
+                                                <p class="card-text">
+                                                    descripcion
+                                                </p>
+
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+                                    
+                                <?php } ?>
+
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-2"></div>
+                </div>
+
+
+
+
+            </div> <!-- container-fluid -->
+        </div>
+        <!-- End Page-content -->
+
+        <?php include("include/pie_pagina.php"); ?>
+
+    </div>
+    <!-- end main content-->
+
+    </div>
+    <!-- END layout-wrapper -->
+
+    <!-- Overlay-->
+    <div class="menu-overlay"></div>
+
+
+    <!-- jQuery  -->
+    <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/bootstrap.bundle.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/metismenu.min.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/waves.js"></script>
+    <script src="plantilla/Admin/vertical/assets/js/simplebar.min.js"></script>
+
+    <!-- App js -->
+    <script src="plantilla/Admin/vertical/assets/js/theme.js"></script>
+
+</body>
+
+</html>
